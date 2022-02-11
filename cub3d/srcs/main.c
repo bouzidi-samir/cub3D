89b5774a473c1b019ee7fa3e28442d11243eebc0
Bouzidi Samir <fouleck30@hotmail.fr>
2022-02-11 12:23:36 +0100

    norm in progress

--- cub3d/srcs/main.c
+++ cub3d/srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: samirbouzidi <samirbouzidi@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 14:18:08 by samirbouzid       #+#    #+#             */
-/*   Updated: 2022/02/10 19:18:35 by samirbouzid      ###   ########.fr       */
+/*   Updated: 2022/02/11 12:22:35 by samirbouzid      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-int		check_filename(char *str, t_datastock *datacube)
+int	check_filename(char *str, t_datastock *datacube)
 {
 	int			i;
 
@@ -33,13 +33,14 @@ int		check_filename(char *str, t_datastock *datacube)
 		return (0);
 	else
 	{
-    	ft_putendl_fd("Nom de la map invalide", 2);
-        free_data(datacube);
+		ft_putendl_fd("Nom de la map invalide", 2);
+		free_data(datacube);
 		return (1);
-    }
+	}
 }
 
-/* je parcour mon fichier et parse ligne par ligne si une erreur est détecter j'incremente la variable error et sors de ma boucle*/
+// je parcour mon fichier et parse ligne par ligne si une erreur est détecter 
+// j'incremente la variable error et sors de ma boucle*/
 
 int	ft_parsing(char *file, t_datastock *datacube)
 {
@@ -49,9 +50,12 @@ int	ft_parsing(char *file, t_datastock *datacube)
 
 	ret = 1;
 	str = NULL;
-	if ((fd = open(file, O_DIRECTORY)) != -1)
+	fd = open(file, O_DIRECTORY);
+	if (fd != -1)
 		ft_putendl_fd("error : the argument is not a file", 1);
-	if ((fd = open(file, O_RDONLY)) == -1)
+	fd = 0;
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
 		ft_putendl_fd("error invalide file", 2);
 	datacube->error = 0;
 	while (ret != 0)
@@ -67,7 +71,7 @@ int	ft_parsing(char *file, t_datastock *datacube)
 }
 //fonction qui dessine une image entière et lit les données à chaque fois.
 
-int start_game(t_datastock *datacube)
+int	start_game(t_datastock *datacube)
 {	
 	ft_init_raycast(datacube);
 	datacube->mlx_ptr = mlx_init();
@@ -76,40 +80,44 @@ int start_game(t_datastock *datacube)
 		ft_error(datacube, 3);
 		return (1);
 	}
-	datacube->three_d.img = mlx_new_image(datacube->mlx_ptr, datacube->rx, datacube->ry);
-	datacube->three_d.img_data = (int *)mlx_get_data_addr(datacube->three_d.img, &datacube->three_d.
-			bpp, &datacube->three_d.size_line, &datacube->three_d.endian);
-	datacube->three_d.img2 = mlx_new_image(datacube->mlx_ptr, datacube->rx, datacube->ry);
-	datacube->three_d.img_data2 = (int *)mlx_get_data_addr(datacube->three_d.img2, &datacube->
-			three_d.bpp, &datacube->three_d.size_line, &datacube->three_d.endian);
-	datacube->mlx_win = mlx_new_window(datacube->mlx_ptr, datacube->rx, datacube->ry, "Hello world!");
+	datacube->three_d.img
+		= mlx_new_image(datacube->mlx_ptr, datacube->rx, datacube->ry);
+	datacube->three_d.img_data = (int *)mlx_get_data_addr
+		(datacube->three_d.img, &datacube->three_d.bpp,
+			&datacube->three_d.size_line, &datacube->three_d.endian);
+	datacube->three_d.img2
+		= mlx_new_image(datacube->mlx_ptr, datacube->rx, datacube->ry);
+	datacube->three_d.img_data2 = (int *)mlx_get_data_addr
+		(datacube->three_d.img2, &datacube->three_d.bpp,
+			&datacube->three_d.size_line, &datacube->three_d.endian);
+	datacube->mlx_win = mlx_new_window(datacube->mlx_ptr, datacube->rx,
+			datacube->ry, "Hello world!");
 	ft_raycasting(datacube);
 	mlx_hook(datacube->mlx_win, 17, 0, ft_exit, datacube);
 	mlx_hook(datacube->mlx_win, 2, 1L << 0, key_press, datacube);
 	mlx_loop_hook(datacube->mlx_ptr, ft_raycasting, datacube);
 	mlx_hook(datacube->mlx_win, 3, 1L << 1, key_release, datacube);
-	//mlx_hook(datacube->mlx_win, 33, 1L << 17, ft_exit, datacube);
 	mlx_loop(datacube->mlx_ptr);
 	return (0);
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    t_datastock *datacube;
-    
-    datacube = malloc(sizeof(*datacube));
+	t_datastock	*datacube;
+
+	datacube = malloc(sizeof(*datacube));
 	if (datacube == NULL)
 		return (-1);
-    ft_init_datastock(datacube);
+	ft_init_datastock(datacube);
 	if (ac == 2)
 	{
-        if (check_filename(av[1], datacube) == 1)
-            return (1);
-        ft_parsing(av[1], datacube);        
+		if (check_filename(av[1], datacube) == 1)
+			return (1);
+		ft_parsing(av[1], datacube);
 		if (check_error(av[1], datacube) == 1 || start_game(datacube) == 1)
 			return (1);
 	}
-    else
+	else
 		ft_putendl_fd("Error: You must give one argument", 2);
 	return (0);
 }
