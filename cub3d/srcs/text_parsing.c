b48e439b2d81e46ab8ce90a0429907187c066715
Bouzidi Samir <fouleck30@hotmail.fr>
2022-02-11 14:36:28 +0100

    text_parse in norm

--- cub3d/srcs/text_parsing.c
+++ cub3d/srcs/text_parsing.c
@@ -6,7 +6,7 @@
 /*   By: samirbouzidi <samirbouzidi@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 15:13:31 by samirbouzid       #+#    #+#             */
-/*   Updated: 2022/02/11 14:09:01 by samirbouzid      ###   ########.fr       */
+/*   Updated: 2022/02/11 14:33:34 by samirbouzid      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /* Gestion d'erreur et parsing du path dans la stucture */
 
-int		ft_get_path(char *str, char **texture, t_datastock *datacube, int j)
+int	ft_get_path(char *str, char **texture, t_datastock *datacube, int j)
 {
 	datacube->path_size = 0;
 	if (*texture != NULL)
@@ -23,7 +23,7 @@ int		ft_get_path(char *str, char **texture, t_datastock *datacube, int j)
 		return (0);
 	}
 	if (ft_check_char(str, '.') == 0 || ft_check_char(str, '/') == 0
-			|| ft_strlen2(str) <= 2)
+		|| ft_strlen2(str) <= 2)
 		datacube->error = 2;
 	while (str[j] != '.')
 	{
@@ -31,8 +31,7 @@ int		ft_get_path(char *str, char **texture, t_datastock *datacube, int j)
 			datacube->error = 2;
 		j++;
 	}
-	if (!(*texture = (char *)(malloc(sizeof(char) * (ft_strlen2(str) + 1)))))
-		datacube->error = 2;
+	*texture = (char *)(malloc(sizeof(char) * (ft_strlen2(str) + 1)));
 	while (str[j] != '\0')
 	{
 		(*texture)[datacube->path_size] = str[j];
@@ -45,9 +44,9 @@ int		ft_get_path(char *str, char **texture, t_datastock *datacube, int j)
 
 /* Detection et parsing des textures */
 
-void check_texture(char *str, t_datastock *datacube)
+void	check_texture(char *str, t_datastock *datacube)
 {
-    int			i;
+	int			i;
 	static int	j = 0;
 
 	i = 0;
@@ -60,8 +59,8 @@ void check_texture(char *str, t_datastock *datacube)
 	else if (str[i] == 'W' && str[i + 1] == 'E')
 		ft_get_path(str, &datacube->we, datacube, 2);
 	else if (str[0] != 'N' && str[0] != 'S' && str[0] != 'W' && str[0] != 'E'
-			&& str[0] != 'F' && str[0] != 'C'
-			&& str[0] > 65 && str[0] < 122)
+		&& str[0] != 'F' && str[0] != 'C'
+		&& str[0] > 65 && str[0] < 122)
 		datacube->error = 2;
 	else if (str[i] == 'S' && str[i + 1] != '0')
 		datacube->error = 2;
@@ -86,22 +85,18 @@ void	ft_get_texture_adress(t_datastock *datacube)
 
 int	texture_parse(t_datastock *datacube)
 {
-	if (!(datacube->text[0].img = mlx_xpm_file_to_image(datacube->mlx_ptr,
-					datacube->no, &(datacube->text[0].width),
-					&(datacube->text[0].height))))
-		return (1);
-	if (!(datacube->text[1].img = mlx_xpm_file_to_image(datacube->mlx_ptr,
-					datacube->so, &(datacube->text[1].width),
-					&(datacube->text[1].height))))
-		return (1);
-	if (!(datacube->text[2].img = mlx_xpm_file_to_image(datacube->mlx_ptr,
-					datacube->we, &(datacube->text[2].width),
-					&(datacube->text[2].height))))
-		return (1);
-	if (!(datacube->text[3].img = mlx_xpm_file_to_image(datacube->mlx_ptr,
-					datacube->ea, &(datacube->text[3].width),
-					&(datacube->text[3].height))))
-		return (1);
+	datacube->text[0].img = mlx_xpm_file_to_image(datacube->mlx_ptr,
+			datacube->no, &(datacube->text[0].width),
+			&(datacube->text[0].height));
+	datacube->text[1].img = mlx_xpm_file_to_image(datacube->mlx_ptr,
+			datacube->so, &(datacube->text[1].width),
+			&(datacube->text[1].height));
+	datacube->text[2].img = mlx_xpm_file_to_image(datacube->mlx_ptr,
+			datacube->we, &(datacube->text[2].width),
+			&(datacube->text[2].height));
+	datacube->text[3].img = mlx_xpm_file_to_image(datacube->mlx_ptr,
+			datacube->ea, &(datacube->text[3].width),
+			&(datacube->text[3].height));
 	ft_get_texture_adress(datacube);
 	return (0);
 }
