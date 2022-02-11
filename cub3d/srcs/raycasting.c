07dd70c789de93a23a634bf5e9df0bb1a6bb93e2
Bouzidi Samir <fouleck30@hotmail.fr>
2022-02-11 21:37:32 +0100

    ray norm ok

--- cub3d/srcs/raycasting.c
+++ cub3d/srcs/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: samirbouzidi <samirbouzidi@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 14:19:05 by samirbouzid       #+#    #+#             */
-/*   Updated: 2022/02/10 19:22:12 by samirbouzid      ###   ########.fr       */
+/*   Updated: 2022/02/11 21:33:41 by samirbouzid      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,22 @@ void	ft_draw_distance(t_datastock *datacube)
 {
 	if (datacube->raycast.side == 0)
 		datacube->raycast.perpwalldist = ((double)datacube->raycast.mapx - \
-				datacube->raycast.posx + (1 - (double)datacube->raycast.
-				stepx) / 2) / datacube->raycast.raydirx;
+				datacube->raycast.posx + (1 - (double)datacube->raycast.stepx)
+				/ 2) / datacube->raycast.raydirx;
 	else
 		datacube->raycast.perpwalldist = ((double)datacube->raycast.mapy - \
-				datacube->raycast.posy + (1 - (double)datacube->raycast.
-				stepy) / 2) / datacube->raycast.raydiry;
-	datacube->raycast.lineheight = (int)(datacube->ry / datacube->raycast.perpwalldist);
-	datacube->raycast.drawstart = -datacube->raycast.lineheight / 2 + datacube->ry / 2;
+				datacube->raycast.posy + (1 - (double)datacube->raycast.stepy)
+				/ 2) / datacube->raycast.raydiry;
+	datacube->raycast.lineheight = (int)(datacube->ry
+			/ datacube->raycast.perpwalldist);
+	datacube->raycast.drawstart = -datacube->raycast.lineheight
+		/ 2 + datacube->ry / 2;
 	if (datacube->raycast.drawstart < 0)
 		datacube->raycast.drawstart = 0;
-	datacube->raycast.drawend = datacube->raycast.lineheight / 2 + datacube->ry / 2;
-	if (datacube->raycast.drawend >= datacube->ry || datacube->raycast.drawend < 0)
+	datacube->raycast.drawend = datacube->raycast.lineheight
+		/ 2 + datacube->ry / 2;
+	if (datacube->raycast.drawend >= datacube->ry
+		|| datacube->raycast.drawend < 0)
 		datacube->raycast.drawend = datacube->ry - 1;
 }
 
@@ -51,7 +55,8 @@ void	ft_ray_progress(t_datastock *datacube)
 			datacube->raycast.mapy += datacube->raycast.stepy;
 			datacube->raycast.side = 1;
 		}
-		if (datacube->map[datacube->raycast.mapx][datacube->raycast.mapy] == '1')
+		if (datacube->map[datacube->raycast.mapx][datacube->raycast.mapy]
+			== '1')
 			datacube->raycast.hit = 1;
 	}
 	ft_draw_distance(datacube);
@@ -62,52 +67,50 @@ void	ft_get_distance(t_datastock *datacube)
 	if (datacube->raycast.raydirx < 0)
 	{
 		datacube->raycast.stepx = -1;
-		datacube->raycast.sidedistx = (datacube->raycast.posx - datacube->raycast.mapx) \
-							* datacube->raycast.deltadistx;
+		datacube->raycast.sidedistx = (datacube->raycast.posx
+				- datacube->raycast.mapx) * datacube->raycast.deltadistx;
 	}
 	else
 	{
 		datacube->raycast.stepx = 1;
-		datacube->raycast.sidedistx = (datacube->raycast.mapx + 1.0 - datacube->raycast.posx) \
-							* datacube->raycast.deltadistx;
+		datacube->raycast.sidedistx = (datacube->raycast.mapx + 1.0
+				- datacube->raycast.posx) * datacube->raycast.deltadistx;
 	}
 	if (datacube->raycast.raydiry < 0)
 	{
 		datacube->raycast.stepy = -1;
-		datacube->raycast.sidedisty = (datacube->raycast.posy - datacube->raycast.mapy) \
-							* datacube->raycast.deltadisty;
+		datacube->raycast.sidedisty = (datacube->raycast.posy
+				- datacube->raycast.mapy) * datacube->raycast.deltadisty;
 	}
 	else
 	{
 		datacube->raycast.stepy = 1;
-		datacube->raycast.sidedisty = (datacube->raycast.mapy + 1.0 - datacube->raycast.posy) \
-							* datacube->raycast.deltadisty;
+		datacube->raycast.sidedisty = (datacube->raycast.mapy + 1.0
+				- datacube->raycast.posy) * datacube->raycast.deltadisty;
 	}
 	ft_ray_progress(datacube);
 }
 
 // fonction de raycasting qui passe par tous les x
 
-int		ft_raycasting(t_datastock *datacube)
+int	ft_raycasting(t_datastock *datacube)
 {
 	datacube->raycast.x = 0;
 	while (datacube->raycast.x < datacube->rx)
 	{
 		ft_init_raycast2(datacube);
 		ft_get_distance(datacube);
-		ft_col_draw(datacube);		
+		ft_col_draw(datacube);
 		datacube->raycast.x++;
 	}
 	mlx_put_image_to_window(datacube->mlx_ptr, datacube->mlx_win,
-			datacube->three_d.img, 0, 0);
-	//init_minimap(datacube);
-	
+		datacube->three_d.img, 0, 0);
 	front_back(datacube);
 	ft_left_right(datacube);
 	rotate_right_left(datacube);
 	init_minimap(datacube);
-	mlx_put_image_to_window(datacube->mlx_ptr,datacube->mlx_win,datacube->mini.img_player, datacube->mini.posx, datacube->mini.posy);
-	//mlx_put_image_to_window(datacube->mlx_ptr, datacube->mlx_win,datacube->mini.img_player, datacube->raycast.posx * datacube->rx_bloc, datacube->raycast.posy * datacube->rx_bloc);
+	mlx_put_image_to_window(datacube->mlx_ptr, datacube->mlx_win,
+		datacube->mini.img_player, datacube->mini.posx, datacube->mini.posy);
 	ft_swap(datacube);
 	return (0);
 }
