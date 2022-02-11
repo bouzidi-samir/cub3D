cde0982636ac1e387ea7b74eb78e72a786f3f98c
Bouzidi Samir <fouleck30@hotmail.fr>
2022-02-11 14:53:40 +0100

    map_parse in norm

--- cub3d/srcs/map_parsing.c
+++ cub3d/srcs/map_parsing.c
@@ -6,20 +6,21 @@
 /*   By: samirbouzidi <samirbouzidi@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 11:44:39 by samirbouzid       #+#    #+#             */
-/*   Updated: 2022/02/10 18:14:52 by samirbouzid      ###   ########.fr       */
+/*   Updated: 2022/02/11 14:50:46 by samirbouzid      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-int		get_map(char *str, t_datastock *datacube)
+int	get_map(char *str, t_datastock *datacube)
 {
 	static int	i = 0;
 	int			j;
 
 	j = 0;
 	datacube->map[i] = NULL;
-	if (!(datacube->map[i] = malloc(sizeof(char) * datacube->width + 1)))
+	datacube->map[i] = malloc(sizeof(char) * datacube->width + 1);
+	if (!datacube->map[i])
 		return (0);
 	while (str[j] != '\0')
 	{
@@ -34,26 +35,25 @@ int		get_map(char *str, t_datastock *datacube)
 	return (0);
 }
 
-int		check_character(char *str, t_datastock *datacube)
+int	check_character(char *str, t_datastock *datacube)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (!str)
 		return (0);
-
-    if (ft_check_char(str, '1') == 1 || ft_check_char(str, '0') == 1)
+	if (ft_check_char(str, '1') == 1 || ft_check_char(str, '0') == 1)
 	{
 		while (str[i] != '\0')
 		{
 			if (str[i] != ' ' && str[i] != '0' && str[i] != '1' \
 					&& str[i] != 'N' && str[i] != 'S' \
 					&& str[i] != 'E' && str[i] != 'W' && str[i] != '\n'
-					&& str[i] != '\t')
+				&& str[i] != '\t')
 			{
 				if (datacube->inmap == 1)
 					datacube->bad_char = 2;
-                return (0);
+				return (0);
 			}
 			i++;
 		}
@@ -62,7 +62,8 @@ int		check_character(char *str, t_datastock *datacube)
 	return (0);
 }
 
-/* Je check si ma map contient les bon char et si elle est bien à la fin du fichier et je recup sa largeur et sa hauteur */
+// Je check si ma map contient les bon char et si elle est bien à la fin 
+// je recup sa largeur et sa hauteur 
 
 void	ft_check_map(char *str, t_datastock *datacube)
 {
@@ -71,13 +72,14 @@ void	ft_check_map(char *str, t_datastock *datacube)
 	static int	width = 0;
 
 	i = 0;
-    if (check_character(str, datacube) == 1)
+	if (check_character(str, datacube) == 1)
 	{
-		if (datacube->f == -1 || datacube->c == -1 || datacube->no == NULL ||
-				datacube->so == NULL || datacube->we == NULL ||
-				datacube->ea == NULL)
+		if (datacube->f == -1 || datacube->c == -1 || datacube->no == NULL
+			|| datacube->so == NULL || datacube->we == NULL
+			|| datacube->ea == NULL)
 			datacube->error = 2;
-		if ((i = ft_strlen(str)) > width)
+		i = ft_strlen(str);
+		if (i > width)
 			width = i;
 		height = height + 1;
 	}
@@ -85,7 +87,7 @@ void	ft_check_map(char *str, t_datastock *datacube)
 	datacube->width = width;
 }
 
-int		parsing_map(char *fichier, t_datastock *datacube)
+int	parsing_map(char *fichier, t_datastock *datacube)
 {
 	int			fd;
 	int			ret;
@@ -94,15 +96,15 @@ int		parsing_map(char *fichier, t_datastock *datacube)
 	ret = 1;
 	str = NULL;
 	fd = open(fichier, O_RDONLY);
-    if (!(datacube->map = malloc(sizeof(char*) * datacube->height)))
-		return (0);
+	datacube->map = malloc(sizeof(char *) * datacube->height);
 	while (ret != 0)
 	{
 		ret = get_next_line(fd, &str);
-		if (datacube->inmap == 1 && empty_line(str) == 0 &&
-				datacube->count < datacube->height)
+		if (datacube->inmap == 1 && empty_line(str) == 0
+			&& datacube->count < datacube->height)
 			datacube->emptyline = 1;
-		if ((datacube->inmap = check_character(str, datacube)) == 1)
+		datacube->inmap = check_character(str, datacube);
+		if (datacube->inmap == 1)
 		{
 			datacube->count++;
 			get_map(str, datacube);
